e29f8dc3710ed6567e43b5c02b286b1b44037cbe
Marien van Os <marien.van.os@efocus.nl>
2016-04-07 16:43:15 +0200

    text status

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -12,14 +12,16 @@ export default class Panel extends Component {
 
   render() {
     const data = this.props.data;
-    const ranges = this.props.ranges;
+    const ranges = this.props.ranges
+    const type = this.props.type;
     const containerClass = classNames({
-      'circle-container':true,
+      'circle-container': true,
       'no-data': data === null,
       'safe': ranges.isSafe(data),
       'danger': ranges.isDanger(data),
       'warning':  ranges.isWarning(data)
     });
+    const statusText = ranges.statusText(data, type);
 
     return (
       <div className={containerClass}>
@@ -29,7 +31,7 @@ export default class Panel extends Component {
                 <h3>{this.props.title}</h3>
                 <h1>{data}</h1>
                 <p>unit</p>
-                <p className="condition"></p>
+                <p className="condition">{statusText}</p>
               </div>
             </div>
         </div>
