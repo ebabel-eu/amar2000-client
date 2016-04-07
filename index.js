137edacdad80887aa2bd13d118a0bbe443d00c89
Nadjib Amar <nadjib.amar@efocus.nl>
2016-04-07 19:25:14 +0200

    getting node index.js to work

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-// In production (Heroku), node.js can be used to run this static website.
-var server = function() {
-  var http = require('http');
-  var express = require('express');
-  var app = express();
-  var port = 80;
-  var ip = '0.0.0.0';
+(function () {
+    var http = require('http'),
+        express = require('express'),
+        app = express(),
+        port = process.env.PORT || 80,
+        ip = process.env.IP || '0.0.0.0';
 
-  // Set generic headers used in all responses.
-  app.use(function (req, res, next) {
-    res.set({
-      'X-Powered-By': 'NodeJS',
-      'Access-Control-Allow-Methods': 'GET',
-      'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+    // Simple logger.
+    app.use(function (req, res, next) {
+        console.log('%s %s', req.method, req.url);
+        next();
     });
-    next();
-  });
 
-  // Handle all static file GET requests.
-  app.use(express.static(__dirname + '/build'));
+    // Set generic headers used in all responses.
+    app.use(function (req, res, next) {
+        res.set({
+            'X-Powered-By': 'NodeJS',
+            'Access-Control-Allow-Methods': 'GET, POST',               // Allowed request http verbs.
+            'Access-Control-Allow-Headers': 'X-Requested-With,content-type'    // Allowed request headers.
+        });
+        next();
+    });
 
-  http.createServer(
-    app.handle.bind(app)
-  ).listen(port, ip, () => {
-    var logMessage = 'Listening on http://' + ip + ':' + port;
-    console.log(logMessage);
-  });
-};
+    // Handle all static file GET requests.
+    app.use(express.static(__dirname + '/build'));
 
-server();
+    http.createServer(
+        app.handle.bind(app)
+    ).listen(port, ip, function() {
+        console.log('Listening on http://' + ip + ':' + port);
+    });
+}());
